c243151a8e403fc5f403dc51ec5fd1f95d8e1dad
derrick <derricktio36@gmail.com>
2021-01-14 23:06:47 +0700

    login + register

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include<string.h>
 #include<stdlib.h>
 
+char loggedIn[100];
+
 void mainMenu();
 void regis();
 void login();
@@ -57,12 +59,51 @@ void mainMenu(){
     }
 }
 
-void regis(){
-    puts("ini regis");
-    return;
-}
-void login(){
-    puts("ini login");
+//TEMP
+bool checkPassword(const char *name,const char *pass){ return false; }
+bool addUser(){};
+
+void loginMenu(){
+    printf("ini menu login");
+    getchar();
     return;
 }
 
+void regis(){
+    char username[40], password[40];
+
+    printf("Please type in your username [lowercase || 1..24]: ");
+    scanf("%s",username); getchar();
+    printf("Please type in your password [lowercase || 1..24]: ");
+    scanf("%s",password); getchar();
+
+    puts("");
+    puts("--- Registration Successful ---");
+    printf("Press enter to continue");
+    getchar();
+    mainMenu();
+    return;
+}
+
+void login(){
+    char username[40], password[40];
+
+    do{
+        printf("Please type in your username [lowercase || 1..24]: ");
+        scanf("%s",username); getchar();
+        printf("Please type in your password [lowercase || 1..24]: ");
+        scanf("%s",password); getchar();
+
+        if(!checkPassword(username,password)){
+            int temp;
+            puts("Invalid username or password");
+            printf("Try again [ 1 = yes / 0 = no] ? ");
+            scanf("%d",&temp); getchar();
+            if(temp != 1){
+                mainMenu();
+            }
+        }
+    }while(!checkPassword(username,password));
+
+    loginMenu();
+}
